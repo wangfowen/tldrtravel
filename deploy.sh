bed5e4c134d436739c9e76aaee0abaa436d30239
Owen Wang <wangfowen@gmail.com>
2016-01-18 18:31:56 -0800

    fix commit in deploy again

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,7 @@ rm -rf dist/* index.html
 mkdir -p dist
 cp /tmp/bundle.js dist
 cp /tmp/index.html .
-git commit -a -m "Deploy"
+git add dist index.html
+git commit -m "Automated yolo Deploy"
 git push
 git checkout -
