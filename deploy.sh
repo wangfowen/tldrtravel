87c80a69479ce26b333acc36bd48bcfdf99bf9c7
Owen Wang <wangfowen@gmail.com>
2016-01-18 18:25:22 -0800

    Add lulz deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Ensure the repository isn't dirty
+[ `git status -u -s | wc -c` -eq 0 ] \
+      || die "You must commit your changes before deploying."
+
+npm run build
+cp dist/bundle.js index.html /tmp/
+git checkout gh-pages
+rm -rf dist/* index.html
+mkdir -p dist
+cp /tmp/bundle.js dist
+cp /tmp/index.html .
+git commit -A -m "Deploy"
+git push
+git checkout -
